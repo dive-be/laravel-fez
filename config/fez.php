b982c4ae1ce3fc1403160386de449ba8ab74ad2e
Muhammed Sari <muhammed@dive.be>
2021-03-15 17:27:33 +0100

    Get rid of env calls

--- config/fez.php
+++ config/fez.php
@@ -10,13 +10,13 @@ return [
     ],
 
     'defaults' => [
-        'description' => env('FEZ_DEFAULTS_DESCRIPTION'),
-        'image' => env('FEZ_DEFAULTS_IMAGE'),
-        'keywords' => array_filter(explode(',', env('FEZ_DEFAULTS_KEYWORDS'))),
-        'robots' => env('FEZ_DEFAULTS_ROBOTS', 'index, follow'),
-        'separator' => env('FEZ_DEFAULTS_SEPARATOR', ' | '),
-        'suffixed' => true,
-        'title' => env('FEZ_DEFAULTS_TITLE'),
+        'description' => null,
+        'image' => null,
+        'keywords' => null,
+        'robots' => 'index, follow',
+        'separator' => '|',
+        'suffix' => null,
+        'title' => null,
     ],
 
     'features' => [
