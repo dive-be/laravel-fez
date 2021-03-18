3569969627c98a658d949cf2a89fb03d45f1e7ff
Muhammed Sari <muhammed@dive.be>
2021-03-18 12:48:38 +0100

    Hydrate OG

--- src/Containers/OpenGraph.php
+++ src/Containers/OpenGraph.php
@@ -6,7 +6,6 @@ use Dive\Fez\Formatters\TitleFormatter;
 use Dive\Fez\Models\MetaData;
 use Illuminate\Contracts\Routing\UrlGenerator;
 use Illuminate\Support\Arr;
-use Illuminate\Support\Collection;
 
 final class OpenGraph extends Container
 {
@@ -33,6 +32,7 @@ final class OpenGraph extends Container
     {
         return $this
             ->toCollection()
+            ->map(fn ($content, $prop) => $prop === 'title' ? $this->formatter->format($content) : $content)
             ->when($this->locale, fn ($props) => $props->put('locale', $this->activeLocale))
             ->when($this->url, fn ($props) => $props->put('url', $this->urlGenerator->current()))
             ->map(fn ($content, $prop) => '<meta property="'.self::PREFIX.':'.$prop.'" content="'.$content.'" />')
@@ -41,6 +41,11 @@ final class OpenGraph extends Container
 
     public function hydrate(MetaData $data): self
     {
+        $this->properties = array_merge(
+            $this->properties,
+            array_merge(array_filter($data->only('description', 'image', 'title')), $data->open_graph ?? []),
+        );
+
         return $this;
     }
 
