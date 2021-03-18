720095c5e294be46d7d380e2d5ab8a4758a1cd0e
Muhammed Sari <muhammed@dive.be>
2021-03-18 12:38:40 +0100

    Feature specific defaults

--- config/fez.php
+++ config/fez.php
@@ -1,33 +1,50 @@
 <?php
 
-return [
+use Dive\Fez\Fez;
 
-    /**
-     * App-wide defaults. Overwritten if models provide their own defaults.
-     */
+return [
     'defaults' => [
-        'description' => null,
-        'image' => null, // Link to a static asset
-        'keywords' => null, // An array of default keywords
-        'robots' => ['index', 'follow'], // Any valid combination of index/follow noindex/nofollow or all/none
-        'separator' => '|', // Separation symbol used in title generation when a suffix is set
-        'suffix' => null,
-        'title' => null,
+        'general' => [
+            'description' => null,
+            'image' => null, // Relative path or absolute URL
+            'separator' => '|', // Separation symbol used in title generation when a suffix is set
+            'suffix' => env('APP_NAME'),
+            'title' => null,
+        ],
+
+        Fez::FEATURE_META => [
+            'canonical' => true, // Uses Url::current when enabled
+            'keywords' => null, // An array of default keywords
+            'robots' => ['index', 'follow'], // Any valid combination of index/follow, noindex/nofollow or all/none
+        ],
+
+        Fez::FEATURE_OPEN_GRAPH => [
+            'locale' => true, // Generates locale tag based on the current active app locale
+            'site_name' => env('APP_NAME'),
+            'type' => 'website',
+            'url' => true, // Uses Url::current when enabled
+        ],
+
+        Fez::FEATURE_TWITTER_CARDS => [
+            'card' => 'summary',
+            'site' => null, // Prefix with @ mandatory
+        ],
     ],
 
     /**
-     * At least one feature must be enabled. You may leave out features if you don't need it.
+     * At least one feature must be enabled. You may leave out features if you don't need them.
      * The order of these features also defines the order in which the tags are generated when rendering your views.
      */
     'features' => [
-        Dive\Fez\Fez::FEATURE_META,
-        Dive\Fez\Fez::FEATURE_OPEN_GRAPH,
-        Dive\Fez\Fez::FEATURE_TWITTER_CARDS,
+        Fez::FEATURE_META,
+        Fez::FEATURE_OPEN_GRAPH,
+        Fez::FEATURE_TWITTER_CARDS,
         // Dive\Fez\Fez::FEATURE_ALTERNATE_PAGES,
     ],
 
     /**
-     * An array of locales that should be used when generating alternate page tags.
+     * An array of locales the application supports and serves (including the default locale).
+     *
      * At least two locales must be provided when using the alternate pages feature.
      */
     'locales' => [],
--- src/ComponentFactory.php
+++ src/ComponentFactory.php
@@ -8,20 +8,37 @@ use Dive\Fez\Containers\TwitterCards;
 use Dive\Fez\Exceptions\UnexpectedComponentException;
 use Dive\Fez\Localization\AlternatePages;
 use Illuminate\Config\Repository;
+use Illuminate\Contracts\Routing\UrlGenerator;
 use Illuminate\Http\Request;
 
 class ComponentFactory
 {
-    public function __construct(private Repository $config, private Request $request) {}
+    private array $defaults;
+
+    private array $locales;
+
+    public function __construct(
+        private Request $request,
+        private UrlGenerator $url,
+        Repository $config,
+    ) {
+        $this->defaults = $config->get('fez.defaults');
+        $this->locales = $config->get('fez.locales');
+    }
 
     public function make(string $component): Component
     {
         return match ($component) {
-            'alternatePages' => AlternatePages::make(array_unique($this->config->get('fez.locales')), $this->request),
-            'meta' => Meta::make($this->config->get('fez.defaults')),
-            'openGraph' => OpenGraph::make(),
-            'twitterCards' => TwitterCards::make(),
+            Fez::FEATURE_ALTERNATE_PAGES => AlternatePages::make(array_unique($this->locales), $this->request),
+            Fez::FEATURE_META => Meta::make($this->url, $this->defaultsFor(Fez::FEATURE_META)),
+            Fez::FEATURE_OPEN_GRAPH => OpenGraph::make($this->url, $this->defaultsFor(Fez::FEATURE_OPEN_GRAPH)),
+            Fez::FEATURE_TWITTER_CARDS => TwitterCards::make($this->defaultsFor(Fez::FEATURE_TWITTER_CARDS)),
             default => throw UnexpectedComponentException::make($component),
         };
+    }
+
+    private function defaultsFor(string $component): array
+    {
+        return array_merge($this->defaults['general'], $this->defaults[$component]);
     }
 }
