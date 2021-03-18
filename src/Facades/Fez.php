b29ce3c5191e5a9657336d79e3c0233e8d38c9cc
Muhammed Sari <muhammed@dive.be>
2021-03-18 15:51:24 +0100

    Document facade

--- src/Facades/Fez.php
+++ src/Facades/Fez.php
@@ -4,6 +4,14 @@ namespace Dive\Fez\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static string generate()
+ * @method static \Dive\Fez\Component|null get(string $component)
+ * @method static \Dive\Fez\Fez set(array|string $property, $value = null)
+ * @method static array toArray()
+ * @method static \Dive\Fez\Fez use(\Dive\Fez\Contracts\Metaable $metaable)
+ * @method static useBinding(string $binding)
+ */
 class Fez extends Facade
 {
     protected static function getFacadeAccessor()
