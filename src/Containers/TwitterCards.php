ace7499045d3272b9839f717fb2e7fe62056b2a0
Muhammed Sari <muhammed@dive.be>
2021-03-18 13:07:25 +0100

    Hydrate Twitter

--- src/Containers/TwitterCards.php
+++ src/Containers/TwitterCards.php
@@ -2,34 +2,39 @@
 
 namespace Dive\Fez\Containers;
 
+use Dive\Fez\Formatters\TitleFormatter;
 use Dive\Fez\Models\MetaData;
+use Illuminate\Support\Arr;
 
 final class TwitterCards extends Container
 {
     public const PREFIX = 'twitter';
 
+    private TitleFormatter $formatter;
+
+    public function __construct(array $defaults)
+    {
+        $this->formatter = TitleFormatter::make($defaults['suffix'], $defaults['separator']);
+        $this->properties = Arr::except($defaults, ['suffix', 'separator']);
+    }
+
     public function generate(): string
     {
         return $this
             ->toCollection()
+            ->map(fn ($content, $prop) => $prop === 'title' ? $this->formatter->format($content) : $content)
             ->map(fn ($content, $name) => '<meta name="'.self::PREFIX.':'.$name.'" content="'.$content.'" />')
             ->join(PHP_EOL);
     }
 
     public function hydrate(MetaData $data): self
     {
-        return $this;
-    }
+        $this->properties = array_merge(
+            $this->properties,
+            array_merge(array_filter($data->only('description', 'image', 'title')), $data->twitter ?? []),
+        );
 
-    public function toArray(): array
-    {
-        return [
-            'card' => 'summary_large_image',
-            'creator' => '@diveHQ',
-            'description' => 'The guest list and parade of limousines with celebrities',
-            'site' => '@dive',
-            'title' => 'Brands to dive for',
-        ];
+        return $this;
     }
 
     protected function normalizeProperty(string $property): string
