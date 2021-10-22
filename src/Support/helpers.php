d60e7d57a56289177b27c663383e976311256f34
Muhammed Sari <muhammed@dive.be>
2021-10-22 14:06:16 +0200

    Allow set in helper using assoc arrays

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -1,12 +1,14 @@
 <?php
 
-use Dive\Fez\Fez;
-
 if (! function_exists('fez')) {
-    function fez(?string $component = null)
+    function fez(array|string|null $component = null)
     {
         if (is_null($component)) {
             return app(__FUNCTION__);
+        }
+
+        if (is_array($component)) {
+            return app(__FUNCTION__)->set($component);
         }
 
         return app(__FUNCTION__)->get($component);
