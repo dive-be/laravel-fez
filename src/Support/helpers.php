85257021d190b6fc2af0ecd8cc8cc72c5ab82ed7
Muhammed Sari <muhammed@dive.be>
2021-10-28 14:49:15 +0200

    Add helpers for individual features

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -1,16 +1,57 @@
 <?php declare(strict_types=1);
 
-if (! function_exists('fez')) {
-    function fez(array|string|null $component = null)
+use Dive\Fez\Feature;
+
+if (! function_exists('alternate')) {
+    function alternate()
     {
-        if (is_null($component)) {
+        return fez(Feature::alternatePage());
+    }
+}
+
+if (! function_exists('fez')) {
+    function fez(array|string|null $key = null)
+    {
+        if (is_null($key)) {
             return app(__FUNCTION__);
         }
 
-        if (is_array($component)) {
-            return app(__FUNCTION__)->set($component);
+        if (is_array($key)) {
+            return app(__FUNCTION__)->set($key);
         }
 
-        return app(__FUNCTION__)->get($component);
+        return app(__FUNCTION__)->get($key);
+    }
+}
+
+if (! function_exists('meta')) {
+    function meta(?string $key = null)
+    {
+        if (is_null($key)) {
+            return fez(Feature::metaElements());
+        }
+
+        return fez(Feature::metaElements())->get($key);
+    }
+}
+
+if (! function_exists('og')) {
+    function og(?string $key = null)
+    {
+        if (is_null($key)) {
+            return fez(Feature::openGraph());
+        }
+
+        return fez(Feature::openGraph())->get($key);
+    }
+}
+
+if (! function_exists('twitter')) {
+    function twitter(?string $key = null) {
+        if (is_null($key)) {
+            return fez(Feature::twitterCards());
+        }
+
+        return fez(Feature::twitterCards())->get($key);
     }
 }
