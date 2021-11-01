8f9e6326a44cc60a3f87c5bf0ef52a2208c4183a
Muhammed Sari <muhammed@dive.be>
2021-11-01 17:15:26 +0100

    Facade doc blocks

--- src/Facades/Fez.php
+++ src/Facades/Fez.php
@@ -4,9 +4,24 @@ namespace Dive\Fez\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \Dive\Fez\AlternatePage          alternatePage()
+ * @method static \Dive\Fez\FezManager             description(string $description)
+ * @method static \Dive\Fez\FezManager             except(...$features)
+ * @method static \Dive\Fez\FezManager             for(\Dive\Fez\Contracts\Metable $model)
+ * @method static \Dive\Fez\Component              get(string $feature)
+ * @method static \Dive\Fez\FezManager             image(string $image)
+ * @method static \Dive\Fez\MetaElements           metaElements()
+ * @method static \Dive\Fez\Contracts\Metable|null model()
+ * @method static \Dive\Fez\FezManager             only(...$features)
+ * @method static \Dive\Fez\OpenGraph\RichObject   openGraph()
+ * @method static \Dive\Fez\FezManager             set(array|string $property, string|null $value = null)
+ * @method static \Dive\Fez\FezManager             title(string $title)
+ * @method static \Dive\Fez\TwitterCards\Card      twitterCards()
+ */
 class Fez extends Facade
 {
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'fez';
     }
