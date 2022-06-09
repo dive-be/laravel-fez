772a8eeb353cda8acf26efb000b1ed0a7e64099f
Muhammed Sari <muhammed@dive.be>
2022-06-09 22:21:01 +0200

    Update facade docblocks

--- src/Fez.php
+++ src/Fez.php
@@ -5,19 +5,20 @@ namespace Dive\Fez;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static AlternatePage          alternatePage()
- * @method static Manager                description(string $description)
- * @method static Manager                except(...$features)
- * @method static Manager                for(Contracts\Metable $model)
- * @method static Component              get(string $feature)
- * @method static Manager                image(string $image)
- * @method static MetaElements           metaElements()
- * @method static Contracts\Metable|null model()
- * @method static Manager                only(...$features)
- * @method static OpenGraph\RichObject   openGraph()
- * @method static Manager                set(array|string $property, string|null $value = null)
- * @method static Manager                title(string $title)
- * @method static TwitterCards\Card      twitterCards()
+ * @method static Manager|AlternatePage        alternatePage(AlternatePage|null $alternatePage)
+ * @method static Manager                      description(string $description)
+ * @method static Manager                      except(string ...$features)
+ * @method static Component                    get(string $feature)
+ * @method static Manager                      image(string $image)
+ * @method static Manager                      loadFrom(Contracts\Metable $model)
+ * @method static Manager|MetaElements         metaElements(MetaElements|null $metaElements)
+ * @method static Manager                      only(string ...$features)
+ * @method static Manager|OpenGraph\RichObject openGraph(OpenGraph\RichObject|null $richObject)
+ * @method static Manager                      set(array|string $property, string|null $value = null)
+ * @method static Manager                      title(string $title)
+ * @method static Manager|TwitterCards\Card    twitterCards(TwitterCards\Card|null $card)
+ *
+ * @see Manager
  */
 class Fez extends Facade
 {
